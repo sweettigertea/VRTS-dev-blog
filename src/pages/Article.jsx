a0dec7b4f3366296fff465eb3540ed219e324728
sweettigertea <sweettigertea@gmail.com>
2025-04-03 21:07:46 +0000

    Fixed swapped Homepage and Article pages.

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Article = () => {
   return (
-    <div className='w-full bg-[#f9f9f9] py-[50px]'>
-      <div className='max-w-[1240px] mx-auto'>
-        Article Details
-      </div>
+    <div >
+      Article Details
     </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,8 +3,10 @@ import {Link} from "react-router-dom";
 
 export default function Homepage() {
     return (
-        <div>
-            HomePage
+        <div className='w-full bg-[#f9f9f9] py-[50px]'>
+            <div className='max-w-[1240px] mx-auto'>
+                Homepage
+            </div>
         </div>
     );
 };
