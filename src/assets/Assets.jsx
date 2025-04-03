3baf6ecbb64785d0d8c1db17470ea42529baa678
sweettigertea <sweettigertea@gmail.com>
2025-04-03 17:37:46 +0000

    Added basic Navbar layout.

---
+++ src/assets/Assets.jsx
@@ -0,0 +1,11 @@
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import close from './close.svg'
+import menu from './menu.svg'
+
+export {
+    menu,
+    close,
+    github,
+    linkedin,
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {github, linkedin} from '../assets/Assets'
 
-const Navbar = () => {
+
+function Navbar() {
   return (
-    <div>
-      <Link to="/"><h1>SweetTigerTea's DevBlog</h1></Link>
+    <div className='w-screen h-[80px] bg-white fixed drop-shadow-lg'>
+      <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
+        <div>
+          <Link to="/"><h1 className='ml-3 w-full'>SweetTigerTea's DevBlog</h1></Link>
+        </div>
+        <div className='flex items-center'>
+          <ul className='hidden md:flex'>
+            <li className='p-4'>Categories</li>
+            <li className='p-4'>Search</li>
+          </ul>
+        </div>
+        <div className='hidden md:flex mr-3'>
+          <img src={github} alt="Github Logo" className='h-[25px]'/>
+          <img src={linkedin} alt="LinkedIn Logo" className='h-[25px]'/>
+        </div>
+      </div>
     </div>
   )
 }
