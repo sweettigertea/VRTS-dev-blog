b77fbb653a8fce2421b32777eb3c1df6dcdbaf4d
sweettigertea <sweettigertea@gmail.com>
2025-04-04 17:15:20 +0000

    Added routing per article :id to hompage for dynamic routing to articles in our url!

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -33,13 +33,15 @@ export default function Homepage() {
             <div className='max-w-[1240px] mx-auto'>
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 text-black">
                     {articles.map((article)=>
-                        <div key={article.id} className="bg-white rounded-xl overflow-hidden drop-shadow-md">
-                            <img src={article.cover} alt='Image Placeholdersss' className="h-56 w-full object-cover"/>
-                            <div className="p-8">
-                                <h3 className="font-bold text-2xl my-1">{article.title}</h3>
-                                <p className="text-gray-600 text-xl">{article.desc}</p>
+                        <Link key={article.id} to={`/articles/${article.id}`}>
+                            <div className="bg-white rounded-xl overflow-hidden drop-shadow-md">
+                                <img src={article.cover} alt='Image Placeholdersss' className="h-56 w-full object-cover"/>
+                                <div className="p-8">
+                                    <h3 className="font-bold text-2xl my-1">{article.title}</h3>
+                                    <p className="text-gray-600 text-xl">{article.desc}</p>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     )}
                 </div>
             </div>
